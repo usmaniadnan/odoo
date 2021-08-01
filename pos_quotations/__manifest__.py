76b8d7bf6a4d6e91dbcaee50f4fb3cd4a879e6d7
adnan <mdadnanusmani@gmail.com>
2021-08-01 04:38:12 +0300

    first commit

---
+++ pos_quotations/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'POS Quotations',
+    'version': '1.0',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'author': 'pharmola',
+    'summary': "This module allows you to create quotations in pos." ,
+    'description': """
+
+=======================
+
+This module allows you to create quotations in pos.
+
+""",
+    'depends': ['point_of_sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/sequence.xml',
+        'report/quotation_template.xml'
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'images': [
+        'static/description/create_qut.jpg',
+    ],
+    'installable': True,
+    'website': '',
+    'auto_install': False,
+    'price': 70,
+    'currency': 'EUR',
+}
